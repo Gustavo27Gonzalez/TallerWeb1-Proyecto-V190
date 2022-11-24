132284babfd5aa7640f2cd48053dd891330135b8
Meistt <aguslive7@gmail.com>
2022-11-24 00:06:16 -0300

    compra repository test

--- src/test/java/ar/edu/unlam/tallerweb1/repository/CompraRepositoryTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/repository/CompraRepositoryTest.java
@@ -1,22 +1,32 @@
 package ar.edu.unlam.tallerweb1.repository;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 
+import ar.edu.unlam.tallerweb1.SpringTest;
 import ar.edu.unlam.tallerweb1.controller.dtos.DatosCompra;
 import ar.edu.unlam.tallerweb1.models.compra.Compra;
 import ar.edu.unlam.tallerweb1.models.rifas.Rifa;
 import ar.edu.unlam.tallerweb1.models.usuarios.Usuario;
+import ar.edu.unlam.tallerweb1.repository.repositoryImpl.RepositorioCompraImpl;
 
-public class CompraRepositoryTest {
+public class CompraRepositoryTest extends SpringTest{
 	
 	@Autowired
 	private RepositorioCompra repositorioCompra;
+	
 	private Usuario usuario = new Usuario();
+	
+	@Before
+	public void init() {
+		this.repositorioCompra = new RepositorioCompraImpl(this.sessionFactory);
+	}
 	
 	@Test @Transactional @Commit
 	public void alTerminarDeRealizarLaCompraLaMismaEsGuardadaEnLaBaseDeDatos() {
@@ -26,7 +36,8 @@ public class CompraRepositoryTest {
 	}
 
 	private Compra dadoQueTengoUnaCompraListaParaSerPersistida() {
-		Rifa rifa = new Rifa(1L, false);
+		Rifa rifa = new Rifa();
+		rifa.setFueVendida(Boolean.FALSE);
 		usuario = createUser();
 		DatosCompra datosCompra = new DatosCompra(rifa, usuario);
 		return new Compra(datosCompra);
